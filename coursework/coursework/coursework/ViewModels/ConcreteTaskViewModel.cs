83aa272ce55f3dbff1833ef03156ec37978ea323
cneker <staskotashevich@gmail.com>
2022-04-19 01:10:03 +0300

    created concretevm

---
+++ coursework/coursework/coursework/ViewModels/ConcreteTaskViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using coursework.Models;
+using coursework.Services;
+using Xamarin.Forms;
+
+namespace coursework.ViewModels
+{
+    public class ConcreteTaskViewModel : BaseViewModel, IQueryAttributable
+    {
+        private Task _concreteTask;
+        private TaskService _taskService;
+
+
+        public Task ConcreteTask
+        {
+            get => _concreteTask;
+            set
+            {
+                _concreteTask = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ConcreteTaskViewModel()
+        {
+            _taskService = new TaskService();
+        }
+
+        public void ApplyQueryAttributes(IDictionary<string, string> query)
+        {
+            var id = int.Parse(HttpUtility.UrlDecode(query["Id"]));
+        }
+
+        private async void GetTask(int id)
+        {
+            var task = await _taskService.Get(id);
+            if (task != null)
+            {
+                ConcreteTask = task;
+            }
+        }
+    }
+}
